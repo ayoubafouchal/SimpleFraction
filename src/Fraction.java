e0c38e66df4fd59cc6ee45fdf2cc7f2c2d7d8801
Afouchal <ayoub.afouchal@gmail.com>
2020-10-09 14:35:57 +0200

    Fraction vide

---
+++ src/Fraction.java
@@ -0,0 +1,18 @@
+
+public class Fraction {
+
+	private int a; 
+	private int b;
+	
+	public Fraction(int a, int b){
+		this.a = a;
+		this.b = b;
+	}
+
+	@Override
+	public String toString() {
+		return " a=" + a + ", b=" + b + "";
+	}
+	
+	
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Fraction f= new Fraction(1,2);
+		
+		System.out.println(f.toString());
+	}
+
+}
